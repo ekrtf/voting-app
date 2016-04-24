801a67c77ac8b2b4612ad8f5f367d907f274f623
zceiekr <emile@kratiroff.com>
2016-04-24 07:02:10 -0700

    client infrastructure

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,20 @@
 import Server from 'socket.io';
 
-export default function startServer() {
-  const io = new Server().attach(8090);
+export default function startServer(store) {
+    const io = new Server().attach(8090);
+
+    // emit the application state every time it changes
+    store.subscribe(
+        // NB optimizing this could emit the diff only rather
+        // than the whole application state
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    io.on('connection', (socket) => {
+        // emit the application state to any new connection
+        socket.emit('state', store.getState().toJS());
+
+        // listen for client actions
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
